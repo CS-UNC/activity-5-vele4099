67d6ccd7de82e713e7c2b6c3ff7f41a4503fbcfa
vele4099 <142831267+vele4099@users.noreply.github.com>
2025-10-19 03:21:22 +0000

    functiuon1

---
+++ activity5.py
@@ -0,0 +1,35 @@
+words_file = open('CROSSWD.txt','r')
+
+'''prints each word
+for i in words_file: 
+    print(i)
+
+    
+'''
+print([x for x in dir(words_file) if '_' != x[0]])
+
+
+
+''' 
+for i in words_file:
+print(words_file.readline())
+print(i.strip())  
+'''
+
+
+#Write a function twenty_or_more(string)
+#indicating the path to the file to consider and returns all words read from the file that have more than 20 characters as a list. Note that you will have to open the file indicated in file for reading, and iterate over all lines in the file. Also remember that you can add values to a list using the .append(value) method
+def twenty_or_more(file):
+    long_words = []
+
+    for line in words_file:
+        ## Decode Each Line from input file + strip the special characters.
+        word = line.strip()
+        #Check Word Length
+        if word.__len__() > 20: 
+            long_words.append(word)
+    return(long_words)
+
+results = twenty_or_more(words_file)
+
+print(results)
