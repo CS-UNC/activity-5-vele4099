3256fa380c56c2167b8552492f620631eca00797
vele4099 <142831267+vele4099@users.noreply.github.com>
2025-10-24 05:13:16 +0000

    all_uses_only_tst

--- activity5.py
+++ activity5.py
@@ -16,6 +16,16 @@ def has_no_e(word):
     let = 'e'
     return(let not in word)
 
+'''
+3x Uses_Only function:
+uses_only("cat", "at") 
+    ^returns [False] as cat uses 'c' and that letter does not appear in "at"
+uses_only("cat", "tac")
+    ^returns [True] as "CAT" uses 't' 'a' 'c'
+uses_only("ct", "cat)
+    ^returns True as cat uses 'c' and 't' exist in "cat"
+'''
+
 def uses_only(word, letters):
     #Each character from words needs to appear in Letters,
     for let in word:
@@ -24,13 +34,16 @@ def uses_only(word, letters):
             #Letter Exist, but not in "letters" [break-loop]
             return False
     return True
-        
-'''
-Three ex.
-uses_only("cat", "at") 
-    ^returns [False] as cat uses 'c' and that letter does not appear in "at"
-uses_only("cat", "tac")
-    ^returns [True] as cat uses 't' 'a' 'c'
-uses_only("ct", "cat")
-    ^returns True as cat uses 'c' and 't' exist in "cat"
-'''
+
+def all_uses_only(file, letters):
+    valid_words = []
+    words_file = open(file, 'r')
+
+    for line in words_file:
+        ## Copy Previous Code to Open File (returns single word string no spaces)
+        word = line.strip()
+        # Check each word with uses_only; add valid ones to the list
+        if uses_only(word, letters):
+            valid_words.append(word)
+        #return our list
+    return (valid_words)
