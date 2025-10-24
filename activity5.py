7de3f85a6bd28cfc7ddb0d8c47703cda6582e3e3
vele4099 <142831267+vele4099@users.noreply.github.com>
2025-10-24 05:01:21 +0000

    usesOnly_fix

--- activity5.py
+++ activity5.py
@@ -16,9 +16,21 @@ def has_no_e(word):
     let = 'e'
     return(let not in word)
 
-def uses_only(word,letters):
-    for let in letters:
-        #for each letter, check [if] inside string {word}
-        if let not in word:
+def uses_only(word, letters):
+    #Each character from words needs to appear in Letters,
+    for let in word:
+        #for each char in word, check if its in
+        if let not in letters:
+            #Letter Exist, but not in "letters" [break-loop]
             return False
     return True
+        
+'''
+Three ex.
+uses_only("cat", "at") 
+    ^returns [False] as cat uses 'c' and that letter does not appear in "at"
+uses_only("cat", "tac")
+    ^returns [True] as cat uses 't' 'a' 'c'
+uses_only("ct", "cat")
+    ^returns True as cat uses 'c' and 't' exist in "cat"
+'''
